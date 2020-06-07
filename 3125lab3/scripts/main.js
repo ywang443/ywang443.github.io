d25fe6490126eb7c3c97a2642b32d3fc9769b520
ywang443 <32001277+ywang443@users.noreply.github.com>
2020-06-07 04:44:49 -0400

    Create main.js

---
+++ 3125lab3/scripts/main.js
@@ -0,0 +1,513 @@
+
+// This function is called when any of the tab is clicked
+// It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
+
+function openInfo(evt, tabName) {
+
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+
+}
+
+
+
+// generate a checkbox list from a list of products
+// it makes each product name as the label for the checkbos
+
+function populateListProductChoices(slct1, slct2) {
+    var s1 = document.getElementById(slct1);
+    var s2 = document.getElementById(slct2);
+
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+    s2.innerHTML = "";
+
+	// obtain a reduced list of products based on restrictions
+    var optionArray = restrictListProducts(products, slct1.value);
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+// This function is called when the "Add selected items to cart" button in clicked
+// The purpose is to build the HTML to be displayed (a Paragraph)
+// We build a paragraph to contain the list of selected items, and the total price
+
+function selectedItems(){
+
+	var ele = document.getElementsByName("product");
+	var chosenProducts = []	;
+
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";
+
+	// build list of selected item
+	var para = document.createElement("P");
+	para.innerHTML = "You selected : ";
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) {
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
+
+	// add paragraph and total price
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+
+}
+
+//This function is sorting items by category
+function fruitsFilter(slct2){
+	var s1 = document.getElementById("dietSelect").value;
+	var s2 = document.getElementById(slct2);
+
+// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+// obtain a reduced list of products based on restrictions
+	var optionArray = getCategory("fruits", s1);
+
+// for each item in the array, create a checkbox element, each containing information such as:
+// <input type="checkbox" name="product" value="Bread">
+// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+function meatFilter(slct2){
+	var s1 = document.getElementById("dietSelect").value;
+	var s2 = document.getElementById(slct2);
+
+// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+// obtain a reduced list of products based on restrictions
+	var optionArray = getCategory("meat", s1);
+
+// for each item in the array, create a checkbox element, each containing information such as:
+// <input type="checkbox" name="product" value="Bread">
+// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+function bakeryFilter(slct2){
+	var optionArray = [];
+	var s1 = document.getElementById("dietSelect").value;
+	var s2 = document.getElementById(slct2);
+
+	var form = document.myForm
+
+		for (var i = 1; i < form.length; i++) {
+				var child = form.dietSelect[i];
+				if (child.checked == true){
+						console.log({child:child.value})
+						optionArray = getCategory("bakery", child.value)
+				}
+		}
+		console.log(optionArray);
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+	// obtain a reduced list of products based on restrictions
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+function vegetablesFilter(slct2){
+	var optionArray = [];
+	var s1 = document.getElementById("dietSelect").value;
+	var s2 = document.getElementById(slct2);
+
+	var form = document.myForm
+
+		for (var i = 1; i < form.length; i++) {
+				var child = form.dietSelect[i];
+				if (child.checked == true){
+						console.log({child:child.value})
+						optionArray = getCategory("vegetables", child.value)
+				}
+		}
+		console.log(optionArray);
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+	// obtain a reduced list of products based on restrictions
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+function beveragesFilter(slct2){
+	var optionArray = [];
+	var s1 = document.getElementById("dietSelect").value;
+	var s2 = document.getElementById(slct2);
+
+	var form = document.myForm
+
+		for (var i = 1; i < form.length; i++) {
+				var child = form.dietSelect[i];
+				if (child.checked == true){
+						console.log({child:child.value})
+						optionArray = getCategory("beverages", child.value)
+				}
+		}
+		console.log(optionArray);
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+	// obtain a reduced list of products based on restrictions
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+function dairyproductsFilter(slct2){
+	var optionArray = [];
+	var s1 = document.getElementById("dietSelect").value;
+	var s2 = document.getElementById(slct2);
+
+	var form = document.myForm
+
+		for (var i = 1; i < form.length; i++) {
+				var child = form.dietSelect[i];
+				if (child.checked == true){
+						console.log({child:child.value})
+						optionArray = getCategory("dairy products", child.value)
+				}
+		}
+		console.log(optionArray);
+	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+	// obtain a reduced list of products based on restrictions
+
+	// for each item in the array, create a checkbox element, each containing information such as:
+	// <input type="checkbox" name="product" value="Bread">
+	// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+function cookingproductsFilter(slct2){
+	var optionArray = [];
+	var s1 = document.getElementById("dietSelect").value;
+	var s2 = document.getElementById(slct2);
+
+	var form = document.myForm
+
+    for (var i = 1; i < form.length; i++) {
+        var child = form.dietSelect[i];
+        if (child.checked == true){
+            console.log({child:child.value})
+            optionArray = getCategory("cooking products", child.value)
+        }
+    }
+		console.log(optionArray);
+// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+// obtain a reduced list of products based on restrictions
+
+// for each item in the array, create a checkbox element, each containing information such as:
+// <input type="checkbox" name="product" value="Bread">
+// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+}
+
+function allFilter(slct2){
+	var optionArray = [];
+	var s1 = document.getElementById("dietSelect").value;
+	var s2 = document.getElementById(slct2);
+
+	var form = document.myForm
+
+    for (var i = 1; i < form.length; i++) {
+        var child = form.dietSelect[i];
+        if (child.checked == true){
+            console.log({child:child.value})
+            optionArray = getCategory("all", child.value)
+        }
+    }
+		console.log(optionArray);
+// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	s2.innerHTML = "";
+
+// obtain a reduced list of products based on restrictions
+
+// for each item in the array, create a checkbox element, each containing information such as:
+// <input type="checkbox" name="product" value="Bread">
+// <label for="Bread">Bread/label><br>
+
+	for (i = 0; i < optionArray.length; i++) {
+		var prodimg = document.createElement("img");
+		prodimg.src = optionArray[i][3];
+		prodimg.height = 150;
+		prodimg.width = 150;
+		var productName = optionArray[i][0];
+		var productPrice = optionArray[i][1];
+
+		var temp = productName.toString()+" $"+productPrice
+		// create the checkbox and add in HTML DOM
+		var checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+		checkbox.name = "product";
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+
+		// create a label for the checkbox, and also add in HTML DOM
+		var label = document.createElement('label')
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(temp));
+		s2.appendChild(label);
+		s2.appendChild(prodimg);
+
+		// create a breakline node and add in HTML DOM
+		s2.appendChild(document.createElement("br"));
+	}
+
+}
+
+//https://stackoverflow.com/questions/8838648/onchange-event-handler-for-radio-button-input-type-radio-doesnt-work-as-one/8997289
+var rad = document.myForm.dietSelect;
+var prev = null;
+for (var i = 0; i < rad.length; i++) {
+    rad[i].addEventListener('change', function() {
+        (prev) ? console.log({prev: prev.value}): null;
+        if (this !== prev) {
+            prev = this;
+        }
+        console.log({this:this.value})
+        populateListProductChoices(this,'displayProduct')
+    });
+}
