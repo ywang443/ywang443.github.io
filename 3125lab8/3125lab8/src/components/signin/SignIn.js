988fb8f875b4b6d7b9a44cc02bf68274be54a616
ywang443 <32001277+ywang443@users.noreply.github.com>
2020-07-16 21:30:26 -0400

    Create SignIn.js

---
+++ 3125lab8/3125lab8/src/components/signin/SignIn.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+
+class SignIn extends React.Component {
+  render() {
+return (
+<div className='Signin'>
+<div className='sign\_inner'>
+<h1>{this.props.text}</h1>
+<button onClick={this.props.closePopup}>close me</button>
+</div>
+</div>
+);
+}
+}
+
+
+
+export default SignIn;
